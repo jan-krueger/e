08a1e63b4c99829234e812ff478cd88bc9a4958b
sweetcode <jan@krueger-jan.de>
2017-01-08 21:01:05 +0100

    Documentation LoopThreadFactory

--- src/src/main/java/de/SweetCode/e/loop/LoopThreadFactory.java
+++ src/src/main/java/de/SweetCode/e/loop/LoopThreadFactory.java
@@ -3,6 +3,11 @@ package de.SweetCode.e.loop;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * <p>
+ *    The LoopThreadFactory is a {@link ThreadFactory} used by {@link de.SweetCode.e.E} to schedule {@link Loop Loops}.
+ * </p>
+ */
 public class LoopThreadFactory implements ThreadFactory {
 
     private final static String NAME_FORMAT = "e-loop-thread-%s-%d";
@@ -10,6 +15,13 @@ public class LoopThreadFactory implements ThreadFactory {
     private final ThreadGroup group;
     private final AtomicInteger threadNumber = new AtomicInteger(1);
 
+    /**
+     * <p>
+     *    Creates a new instance of LoopThreadFactory. If there has been no {@link SecurityManager} established it will
+     *    use the {@link Thread#getThreadGroup()} of the current thread, otherwise the {@link SecurityManager#getThreadGroup()}
+     *    thread group.
+     * </p>
+     */
     public LoopThreadFactory() {
         SecurityManager s = System.getSecurityManager();
         this.group = (s == null) ? Thread.currentThread().getThreadGroup() : s.getThreadGroup();
