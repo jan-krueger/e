b6b3b3337301507f422610a02019bce50c3f96e7
sweetcode <jan@krueger-jan.de>
2017-01-10 15:48:02 +0100

    Documentation: Priority

--- Engine/src/main/java/de/SweetCode/e/rendering/Priority.java
+++ Engine/src/main/java/de/SweetCode/e/rendering/Priority.java
@@ -3,14 +3,24 @@ package de.SweetCode.e.rendering;
 import de.SweetCode.e.utils.ToString.ToStringBuilder;
 
 /**
- * Priority is a general class used in all
- * cases where the developer can or should
- * provide a priority value.
+ * <p>
+ * An enumeration to pre-define the priorities engine-wide.
+ * </p>
  */
 public enum Priority {
 
+    /**
+     * Lowest priority. Called after {@link Priority#LOW} and {@link Priority#NORMAL}.
+     */
     LOW(-1),
+    /**
+     * Normal priority. Called before {@link Priority#LOW}, but after {@link Priority#HIGH}.
+     */
     NORMAL(0),
+
+    /**
+     * Highest priority. Called before {@link Priority#LOW} and {@link Priority#NORMAL}.
+     */
     HIGH(1);
 
     private final int priority;
@@ -19,6 +29,13 @@ public enum Priority {
         this.priority = priority;
     }
 
+    /**
+     * <p>
+     *    Returns the numeric value of the priority. This makes it easier to compare them.
+     * </p>
+     *
+     * @return The numeric value.
+     */
     public int getPriority() {
         return this.priority;
     }
